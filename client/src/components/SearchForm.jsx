4909dbd521ef98af08c53a9a2e4c0c667af3acf4
krithikafnu <singhkrithika2@gmail.com>
2025-05-07 19:38:44 -0500

    second commit

--- client/src/components/SearchForm.jsx
+++ client/src/components/SearchForm.jsx
@@ -59,12 +59,12 @@ function SearchForm({ onSearch }) {
           </select>
         </span>
         <button type="submit" className="search-button">
-          <img src="/assets/searchicon.png" alt="Search" className="search-icon" />
+          <img src="/assets/search_icon.png" alt="Search" className="search-icon" />
         </button>
         <span className="icons">
-          <img src="/assets/bell.png" alt="Notifications" className="icon bell" />
-          <img src="/assets/gear.png" alt="Settings" className="icon gear" />
-          <img src="/assets/user-profile.jpg" alt="User Profile" className="user-profile" />
+          <img src="/assets/bell_icon.png" alt="Notifications" className="icon bell" />
+          <img src="/assets/gear_icon.png" alt="Settings" className="icon gear" />
+          <img src="/assets/user_profile.jpg" alt="User Profile" className="user-profile" />
         </span>
       </form>
     </div>
@@ -72,81 +72,3 @@ function SearchForm({ onSearch }) {
 }
 
 export default SearchForm;
-
-
-
-
-
-/*import React from 'react';
-import '../styles/n2.css';
-
-function SearchForm() {
-  return (
-    <div className="search-container">
-      <div className="search-header">
-        <h2>Client Directory</h2>
-      </div>
-      <form className="search-form">
-        <span className="form-group name-group">
-          <span className="name-label">Name</span>
-          <input type="text" id="name" className="name-input" placeholder="Enter name" />
-        </span>
-        <span className="form-group birthday-group">
-          <label htmlFor="birthday" className="birthday-label">Birthday</label>
-          <input type="text" id="birthday" className="birthday-input" placeholder="MM / DD / YYYY" />
-        </span>
-        <span className="form-group account-group">
-          <label htmlFor="account-type" className="account-type-label">Account Type</label>
-          <select id="account-type" className="account-type-select">
-            <option value="">All</option>
-            <option>Checking</option>
-            <option>Savings</option>
-          </select>
-        </span>
-        <button type="submit" className="search-button">
-          <img src="/assets/searchicon.png" alt="Search" className="search-icon" />
-        </button>
-        <span className="icons">
-          <img src="/assets/bell.png" alt="Notifications" className="icon bell" />
-          <img src="/assets/gear.png" alt="Settings" className="icon gear" />
-          <img src="/assets/user-profile.jpg" alt="User Profile" className="user-profile" />
-        </span>
-      </form>
-    </div>
-  );
-}
-
-export default SearchForm;*/
-
-/*
-import React, { useState } from 'react';
-import '../styles/n2.css';
-
-function SearchForm({ onSearch }) {
-  const [filters, setFilters] = useState({ name: '', birthday: '', type: '' });
-
-  const handleChange = e => {
-    setFilters({ ...filters, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    onSearch(filters);
-  };
-
-  return (
-    <form className="search-form" onSubmit={handleSubmit}>
-      <input type="text" name="name" placeholder="Name" onChange={handleChange} />
-      <input type="text" name="birthday" placeholder="YYYY-MM-DD" onChange={handleChange} />
-      <select name="type" onChange={handleChange}>
-        <option value="">All</option>
-        <option>Checking</option>
-        <option>Savings</option>
-      </select>
-      <button type="submit">Search</button>
-    </form>
-  );
-}
-
-export default SearchForm;*/
-
--- server/config/database.js
+++ server/config/database.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
 
-// Use the MongoDB Atlas URI you defined
 const MONGODB_URI = 'mongodb+srv://krithikafnu:U4aqwMt8LkLMPGFe@cluster0.obznhog.mongodb.net/clientdb?retryWrites=true';
 
 async function connectToDatabase() {
   try {
-    // Remove the deprecated options - they're not needed in Mongoose 6+
     await mongoose.connect(MONGODB_URI);
     console.log('Connected to MongoDB Atlas');
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
-    // Consider adding process.exit(1) for critical failures if appropriate
   }
 }
 
